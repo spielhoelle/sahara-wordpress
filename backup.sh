a5ebcb717449d8389151b0526165ec00b29da312
Thomas Kuhnert <spielhoelle@posteo.net>
2023-02-18 13:58:29 +0900

    chore: infrastructure stufff

--- backup.sh
+++ backup.sh
@@ -1,6 +1,9 @@
 echo "Backup DB"
-ssh $wh docker exec sahara-wordpress_db_1 /bin/bash -c "mysqldump -uwordpress -psomewordpress sahara > /sahara-$(date +%Y-%m-%d).sql"
-dumpfile=$(ssh $wh docker exec sahara-wordpress_db_1 ls / | grep ".sql" | head -n 1)
-ssh $wh docker cp sahara-wordpress_db_1:$dumpfile .
+db=sahara
+ssh $wh docker exec globalmysql /bin/bash -c "mysqldump -uroot -psomewordpress $db > /$db-$(date +%Y-%m-%d).sql"
+dumpfile=$(ssh $wh docker exec globalmysql ls / | grep ".sql" | head -n 1)
+ssh $wh docker cp globalmysql:$dumpfile .
 scp -rp $wh:~/$dumpfile .
+docker exec gmysql /bin/bash -c "mysqldump -uroot -pwordpress $db > $db.sql"
+docker cp gmysql:$db.sql .
 echo "Done"
--- restore.sh
+++ restore.sh
@@ -1,12 +1,17 @@
 # # local
 echo "Replace wordpress domain in dumpfile"
-dumpfile=$(ls | grep ".sql" | tail -n 1)
-OLD_URL=http://sahara.tmy.io
-NEW_URL=http://localhost:8000
-rm -f dump_updated.sql
-sed "s~$OLD_URL~$NEW_URL~g" $dumpfile >'dump_updated.sql'
-docker cp dump_updated.sql sahara-wordpress-db-1:/
-docker exec sahara-wordpress-db-1 /bin/bash -c "mysql -uwordpress -pwordpress sahara < /dump_updated.sql"
+# dumpfile=$(ls -t | grep "sahara" | grep ".sql" | head -n 1)
+# OLD_URL=http://sahara.tmy.io
+# OLD_URL2=http://sahara.tmy.io
+# NEW_URL=http://localhost:8000
+# echo $dumpfile
+# rm -f dump_updated.sql
+# sed "s~$OLD_URL~$NEW_URL~g" $dumpfile >'dump_updated.sql'
+# sed "s~$OLD_URL2~$NEW_URL~g" 'dump_updated.sql' >'dump_updated.sql'
+sed 's,https://sahara.tmy.io,http://localhost:8000,g' sahara-2023-02-16.sql >dump_updated2.sql
+sed 's,http://sahara.tmy.io,http://localhost:8000,g' dump_updated2.sql >dump_updated.sql
+docker cp dump_updated.sql gmysql:/
+docker exec gmysql /bin/bash -c "mysql -uroot -ppassword sahara < /dump_updated.sql"
 
 # # remote
 # dumpfile=$(ls | grep ".sql" | tail -n 1)
